aa28884e51d4e6f4e16dd4f2eba3754ad9c83f94
Marcos A. Campos <marcos@mktecnologia.net.br>
2025-01-19 13:11:41 -0300

    Add FAQ javascript Exercício 0302 Animais Fantásticos

--- JAVASCRIPT/animais-fantasticos/src/js/script.js
+++ JAVASCRIPT/animais-fantasticos/src/js/script.js
@@ -1,28 +1,61 @@
 // INICIO => TABMENU
-//SELECIONANDO TODOS OS "LI" DO ELEMENTO
-const tabmenu = document.querySelectorAll(".js-tabmenu li");
-//SELECIONANDO TODOS OS "SECTION" DO ELEMENTO
-const tabdesc = document.querySelectorAll(".js-tabdesc section");
-// console.log(tabmenu, tabdesc);
+function initTabNav() {
+    // SELECIONANDO TODOS OS "LI" DO ELEMENTO
+    const tabmenu = document.querySelectorAll(".js-tabmenu li");
+    const activeClass = "ativo";
+    //SELECIONANDO TODOS OS "SECTION" DO ELEMENTO
+    const tabdesc = document.querySelectorAll(".js-tabdesc section");
+    // console.log(tabmenu, tabdesc);
 
-//VERIFICA SE OS ELEMENTOS EXISTEM E ADD ATIVO NO PRIMEIRO ITEM
-if ((tabmenu.length) && (tabdesc.length)) {
-    tabdesc[0].classList.add("ativo");
+    //VERIFICA SE OS ELEMENTOS EXISTEM E ADD ATIVO NO PRIMEIRO ITEM
+    if ((tabmenu.length) && (tabdesc.length)) {
+        tabdesc[0].classList.add(activeClass);
+
+        // FUNCTION ADD E REMOVE CLASSE ATIVO DOS ELEMENTOS
+        function activeTab(index) {
+            //REMOVE CLASSE ATIVO
+            tabdesc.forEach((section) => {
+                section.classList.remove(activeClass);
+            });
+            //ADD CLASSE ATIVO
+            tabdesc[index].classList.add(activeClass);
+        }
+
+        //AO CLICAR NA IMAGEM ADD INDICE A FUNC ACTIVETAB
+        tabmenu.forEach((itemmenu, index) => {
+            itemmenu.addEventListener("click", () => {
+                activeTab(index);
+            });
+        });
+    }
 }
 
-// FUNCTION ADD E REMOVE CLASSE ATIVO DOS ELEMENTOS
-function activeTab(index) {
-    //REMOVE CLASSE ATIVO
-    tabdesc.forEach((section) => {
-        section.classList.remove("ativo");
-    });
-    //ADD CLASSE ATIVO
-    tabdesc[index].classList.add("ativo");
+initTabNav();
+//FIM => TABMENU
+
+//INICIO => FAQ ACCORDION
+function faqAccordion() {
+    //SELECIONA O ELEMENTO
+    const accordionList = document.querySelectorAll(".js-accordion dt");
+    const activeClass = "ativo";
+    //VERIFICA SE EXISTE O LEMENTO
+    if (accordionList.length) {
+        //DEIXA O PRIMEIRO ITEM COMO ATIVO
+        accordionList[0].classList.add(activeClass);
+        accordionList[0].nextElementSibling.classList.add(activeClass);
+
+        //ADD CLASSES AO ELEMENTO
+        function activeAccordion() {
+            this.classList.toggle(activeClass);
+            this.nextElementSibling.classList.toggle(activeClass);
+        }
+
+        //EXECUTA A FUNCAO AO CLICAR
+        accordionList.forEach((accordion) => {
+            accordion.addEventListener("click", activeAccordion);
+        })
+    }
 }
 
-//AO CLICAR NA IMAGEM ADD INDICE A FUNC ACTIVETAB
-tabmenu.forEach((itemmenu, index) => {
-    itemmenu.addEventListener("click", () => {
-        activeTab(index);
-    });
-});
+faqAccordion();
+//FIM => FAQ ACCORDION
